38fb06f028fda06695ec18399f8223922ba682cd
Alex Covian <alejandrocovian3@gmail.com>
2018-07-25 16:06:39 -0700

    updated function

--- myexample.py
+++ myexample.py
@@ -1,4 +1,8 @@
 st = 'Print only the words that start with s in this sentence'
 for word in st.split():
-    if word.startswith('s'):
+    if word[0].lower() == 's':
         print(word)
+
+for num in range(0,11):
+    if num % 2 == 0:
+        print (num)
