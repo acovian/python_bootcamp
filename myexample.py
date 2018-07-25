dc88d9754554613754aa07f1afd2e6c1d7fa0a11
Alex Covian <alejandrocovian3@gmail.com>
2018-07-25 16:09:19 -0700

    added even number assignment

--- myexample.py
+++ myexample.py
@@ -3,6 +3,12 @@ for word in st.split():
     if word[0].lower() == 's':
         print(word)
 
-for num in range(0,11):
-    if num % 2 == 0:
+for num in range(0,11,2):
         print (num)
+
+[x for x in range(1,51) if x%3 == 0]
+
+st = 'Print every word in this sentence that has an even number of letters'
+for word in st.split():
+    if len(word)%2==0:
+        print(word)
