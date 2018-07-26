e52aa1114081214eea35383fd8895787333d2dcd
Alex Covian <alejandrocovian3@gmail.com>
2018-07-26 16:06:13 -0700

    added makes_twenty assignment

---
+++ twenty.py
@@ -0,0 +1,9 @@
+def makes_twenty(num1, num2):
+    if num1 == 20 or num2 == 20:
+        print('True')
+    elif num1 + num2 == 20:
+        print('True')
+    else:
+        print('False')
+
+makes_twenty(2,3)
