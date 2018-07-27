da838860069462a31fc21303fad98356e372ee0e
Alex Covian <alejandrocovian3@gmail.com>
2018-07-27 15:52:45 -0700

    added join f to yoda.py

--- yoda.py
+++ yoda.py
@@ -7,6 +7,6 @@
 def master_yoda(text):
     wordlist = text.split()
     reverse_word_list = wordlist[::-1]
-    print( reverse_word_list )
+    print( ' '.join(reverse_word_list).lower() )
 
 master_yoda('We are ready')
