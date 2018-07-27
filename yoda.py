10b50716394bebe6c652fe157480530766e46c25
Alex Covian <alejandrocovian3@gmail.com>
2018-07-27 15:48:03 -0700

    added improved yoda function

--- yoda.py
+++ yoda.py
@@ -1,7 +1,12 @@
-def master_yoda(string1):
-    a = string1.split()
-    a.reverse()
-    result = " ".join(a)
-    print(result)
+# def master_yoda(string1):
+#     a = string1.split()
+#     a.reverse()
+#     result = " ".join(a)
+#     print(result)
+
+def master_yoda(text):
+    wordlist = text.split()
+    reverse_word_list = wordlist[::-1]
+    print( reverse_word_list )
 
 master_yoda('We are ready')
