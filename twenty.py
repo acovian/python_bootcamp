2badd664248809ab2505d528ffe639806924420b
Alex Covian <alejandrocovian3@gmail.com>
2018-07-27 15:37:32 -0700

    added makes_twenty improved function

--- twenty.py
+++ twenty.py
@@ -1,9 +1,13 @@
-def makes_twenty(num1, num2):
-    if num1 == 20 or num2 == 20:
-        print('True')
-    elif num1 + num2 == 20:
-        print('True')
-    else:
-        print('False')
+# def makes_twenty(num1, num2):
+#     if num1 == 20 or num2 == 20:
+#         print('True')
+#     elif num1 + num2 == 20:
+#         print('True')
+#     else:
+#         print('False')
+#
+# makes_twenty(2,3)
 
-makes_twenty(2,3)
+def makes_twenty(n1,n2):
+    return (n1+n2) == 20 or n1 == 20 or n2 == 20
+ makes_twenty(20, 3)
