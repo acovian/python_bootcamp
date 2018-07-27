79dde2ff7a73916cfebb342105a81a4382b5ba01
Alex Covian <alejandrocovian3@gmail.com>
2018-07-27 15:34:25 -0700

    added second animal crackers function

--- crackers.py
+++ crackers.py
@@ -1,7 +1,17 @@
-def animal_crackers(str1, str2):
-    if str1[0] == str2[0]:
-        print('True')
-    else:
-        print('False')
+# def animal_crackers(str1, str2):
+#     if str1[0] == str2[0]:
+#         print('True')
+#     else:
+#         print('False')
+#
+# animal_crackers('Hippo', 'Monkey')
 
-animal_crackers('Hippo', 'Monkey')
+def animal_crackers(text):
+    wordlist = text.lower().split()
+
+    first = wordlist[0]
+    second = wordlist[1]
+
+    return first[0] == second[0]
+
+animal_crackers('Levelheaded Llama')
