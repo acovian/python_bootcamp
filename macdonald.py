7118bd2bdd927a062a45228b7e3fae5aa0cad54f
Alex Covian <alejandrocovian3@gmail.com>
2018-07-27 15:41:50 -0700

    added improved macdonald function

--- macdonald.py
+++ macdonald.py
@@ -1,9 +1,16 @@
-def old_macdonald(name):
-    first_letter = name[0]
-    inbetween = name[1:3]
-    fourth_letter = name[3]
-    rest = name[4:]
+# def old_macdonald(name):
+#     first_letter = name[0]
+#     inbetween = name[1:3]
+#     fourth_letter = name[3]
+#     rest = name[4:]
+#
+#     print(first_letter.upper() + inbetween + fourth_letter.upper() + rest)
 
-    print(first_letter.upper() + inbetween + fourth_letter.upper() + rest)
+def old_macdonald(name):
+
+    first_half = name[:3]
+    second_half = name[3:]
+
+    print( first_half.capitalize() + second_half.capitalize() )
 
 old_macdonald('macdonald')
