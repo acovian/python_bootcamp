a41079c5a36945d2a0bca424b59cdc92dcf3c7b5
Alex Covian <alejandrocovian3@gmail.com>
2018-07-27 15:39:30 -0700

    added macdonald function

--- macdonald.py
+++ macdonald.py
@@ -1,4 +1,9 @@
 def old_macdonald(name):
-    
+    first_letter = name[0]
+    inbetween = name[1:3]
+    fourth_letter = name[3]
+    rest = name[4:]
+
+    print(first_letter.upper() + inbetween + fourth_letter.upper() + rest)
 
 old_macdonald('macdonald')
