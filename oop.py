e4898acaffe54e6c3695d8e1feeaf1212a2be22f
Alex Covian <alejandrocovian3@gmail.com>
2018-08-01 14:37:40 -0700

    added dog

--- oop.py
+++ oop.py
@@ -26,4 +26,16 @@ class Animal():
     def __init__(self):
         print("ANIMAL CREATED")
 
-myanimal = Animal()
+    def who_am_i(self):
+        print("I am an animal")
+
+    def eat(self):
+        print("I am eating")
+
+class Dog(Animal):
+    #inherit base class
+    def __init__(self):
+        Animal.__init__(self)
+        print("Dog Created")
+
+mydog = Dog()
