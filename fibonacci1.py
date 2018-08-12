f24d386bc5e9c20d69d0116c04e1d829108e34df
Alex Covian <alejandrocovian3@gmail.com>
2018-08-11 17:33:02 -0700

    added fibonacci 1

---
+++ fibonacci1.py
@@ -0,0 +1,17 @@
+def fibSequence(n):
+    assert n > 0
+    series = [1]
+    while len(series) < n:
+        if len(series) == 1:
+            series.append(1)
+        else:
+            series.append(series[-1] + series[-2])
+    for i in range(len(series)):
+        series[i] = str(series[i])
+    return(', '.join(series))
+
+def main():
+    print(fibSequence(int(input('How many numbers do you need? '))))
+
+if __name__ == '__main__':
+    main()
